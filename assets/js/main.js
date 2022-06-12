830dd9cc2277bf387cbe315393c1e40c9168de1f
Mikhail <mihaskru17@gmail.com>
2022-06-12 19:44:00 +0400

    закончили верстку в классе

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 //$(document).ready(function () {
-  //$(".header-navbar__hamburger").on("click", function () {
-    //$(this).toggleClass("active");
-    //$(".header-navbar__list").toggleClass("active");
-  //});
+//$(".header-navbar__hamburger").on("click", function () {
+//$(this).toggleClass("active");
+//$(".header-navbar__list").toggleClass("active");
 //});
-$(document).ready(function(){
-	$('.header-navbar__btn').click(function(){
-		$(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').toggleClass('active');
-	});
+//});
+$(document).ready(function () {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+  });
 });
